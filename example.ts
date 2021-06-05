4d9675f9b4e8600a6050174e5319d5168eabad8a
PawFV <pregonipaw@hotmail.com>
2021-06-05 15:27:19 +0200

    example: improved passing payload.

--- example.ts
+++ example.ts
@@ -3,21 +3,22 @@ import pawx from './store'
 const store = pawx({
   state: {
     count: 0,
-    myNameIs: 'Paw'
+    myNameIs: ''
   },
   mutations: {
     sum: (state, amount: number) => (state.count += amount),
     changeName: (state, newName: string) => (state.myNameIs = newName)
   },
   actions: {
-    makeSum: ({ mutate, deliver }) => mutate('sum', 10),
-    makeChangeName: ({ mutate }) => mutate('changeName', 'Leonardox')
+    makeSum: ({ mutate, deliver }, by: number) => mutate('sum', by),
+    makeChangeName: ({ mutate }, name: string) => mutate('changeName', name)
   }
 })
 
 console.log(store.state)
 store.mutate('changeName', 'Carlos')
-store.deliver('makeSum', 'asd')
+console.log(store.state)
+store.deliver('makeSum', 5)
 console.log(store.state)
 
 export default store
